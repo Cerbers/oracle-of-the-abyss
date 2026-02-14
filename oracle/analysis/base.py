80dead4d161d7036570d7f9b3d5693af5d1398f7
Cerber <cerberkan@gmail.com>
2026-02-14 21:40:11 +0100

    Add base file for future features to test CI

---
+++ oracle/analysis/base.py
@@ -0,0 +1,14 @@
+from oracle.poem_model import Poem
+from oracle.domain_objects import Stanza
+
+
+stanzas = []
+
+def poetic_devices(poem: Poem) -> list[Stanza]:
+    for stanza in poem.stanzas:
+        stanzas.append(stanza)
+    return stanzas
+ 
+
+def anaphora(poem_stanza: Stanza) -> list[str]: 
+    pass
